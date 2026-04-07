44fd53ed3596cb6058c0eb0daf77010916395b53
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-04-07 23:23:50 +0300

    update task2

--- Lab9/Purple/Task2.cs
+++ Lab9/Purple/Task2.cs
@@ -8,78 +8,72 @@ public class Task2 : Purple
     public string[] Output {get;private set;}
     public override string ToString()
     {
-        if (Output == null) return string.Empty;
+        if (Output == null) return "";
         return string.Join(Environment.NewLine,Output);
+
     }
     public override void Review()
     {
+        
         int len = 50;
         int index = 0;
         string[] answer = new string[0];
-        string[] words = Input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-        while (index < words.Length)
+        string[] words = Input.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+        
+        while(index < words.Length)
         {
-            int count = 0;
             int start = index;
+            int count = 0;
             int currentLength = 0;
-            StringBuilder temp = new StringBuilder();
-            
-            while (index < words.Length)
+            while(index < words.Length)
             {
-                if (currentLength == 0)
+                if (count == 0)
                 {
-                    currentLength+=words[index].Length;
+                    currentLength = words[index].Length;
+                    count = 1;
+                    index++;
+                }
+                else if (currentLength + 1 + words[index].Length <= len)
+                {
+                    currentLength += words[index].Length+1;
                     count++;
                     index++;
                 }
-                else
-                {
-                    if (currentLength + 1 + words[index].Length <= len)
-                    {
-                        currentLength += words[index].Length + 1;
-                        count++;
-                        index++;
-                    }
-                    else break;
-                }
+                else break;
             }
 
-            if (count == 1)
-            {
-                temp.Append(words[start]);
-            }
+            StringBuilder str = new StringBuilder();
+
+            if (count == 1) str.Append(words[start]);
             else
             {
-                int letters = 0;
-                for (int i=start; i < start+count; i++)
-                    letters+= words[i].Length;
-                
-                int gaps = count -1;
-                int spaces = len - letters;
-                int needSpaces = spaces / gaps;
-                int extrSpaces = spaces % gaps;
+                int gaps = count - 1;
+                int countSymbols =0;
+                for (int i=start; i<start+count; i++)
+                    countSymbols += words[i].Length;
+                int totalSpace = len - countSymbols;
+                int requiredSpace = totalSpace / gaps;
+                int extraSpace = totalSpace % gaps;
 
-                for (int i=0; i<count; i++)
+                for (int i=start; i<start + count; i++)
                 {
-                    temp.Append(words[start+i]);
-                    if (i < gaps)
+                    str.Append(words[i]);
+                    int spaces = requiredSpace;
+                    if (i != start + count - 1) 
                     {
-                        int spcs = needSpaces;
-                        if (extrSpaces > 0)
+                        if (extraSpace > 0 && i!=start+count-1)
                         {
-                            spcs++;
-                            extrSpaces--;
+                            spaces++;
+                            extraSpace--;
+                            
                         }
-                        temp.Append(' ',spcs);    
+                        str.Append(' ',spaces);
                     }
                 }
             }
-
             Array.Resize(ref answer, answer.Length+1);
-            answer[^1] = temp.ToString();
+            answer[^1] = str.ToString();
         }
-
         Output = answer;
     }
 }
