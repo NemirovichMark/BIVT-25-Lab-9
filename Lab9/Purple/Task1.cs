a981c4ab0aaa8d59ff5c9c54425a6e6dfc00ec40
Maksonchik <maksonchik@Maksonchiks-MacBook-Air.local>
2026-04-07 22:00:29 +0300

    update task1

--- Lab9/Purple/Task1.cs
+++ Lab9/Purple/Task1.cs
@@ -1,51 +1,37 @@
-namespace Lab9.Purple
+using System.Text;
+
+namespace Lab9.Purple;
+
+public class Task1 : Purple
 {
-    public class Task1 : Purple
+    public Task1(string input) : base(input) {}
+    public string Output {get; private set;}
+    public override string ToString() => Output;  
+    public override void Review()
     {
-        public Task1(string input) : base(input) {}
-        public string Output {get; private set;}
-        public override string ToString() => Output;  
-        public override void Review()
+        int index=0;
+        string[] words = Input.Split();
+        StringBuilder crypto = new StringBuilder();
+        while (index < words.Length)
         {
-            char[] symbols = Input.ToCharArray();
-            int index = 0;
-            while (index < symbols.Length)
-            {
-                if (!char.IsWhiteSpace(symbols[index]))
-                {
-                    int left = index, right = index;
-                    while ( right < symbols.Length && 
-                            !char.IsWhiteSpace(symbols[right]))
-                    {right++;}
-                    right--;
-
-                    char[] word = new char[right-left+1];
-                    Array.Copy(symbols,left,word,0,right-left+1);
-
-                    ReverseWord(word);
-
-                    Array.Copy(word,0,symbols,left,word.Length);
-                    index = right+2;
-                }
-                else index++;
-            }
-            Output = new string (symbols);
-        }
-        private void ReverseWord(char[] word)
-        {
+            char[] word = words[index].ToCharArray();
             bool hasLetter = word.Any(char.IsLetter);
             bool hasDigit = word.Any(char.IsDigit);
-
             if (hasLetter && !hasDigit)
             {
-                int firstLetterIndex = Array.FindIndex(word, char.IsLetter);
-                int lastLetterIndex = Array.FindLastIndex(word, char.IsLetter);
-
-                if (firstLetterIndex >= 0 && lastLetterIndex >= firstLetterIndex)
+                int indexFirstLetter = Array.FindIndex(word,char.IsLetter);
+                int indexLastLetter = Array.FindLastIndex(word,char.IsLetter);
+                if (indexFirstLetter <= indexLastLetter)
                 {
-                    Array.Reverse(word, firstLetterIndex, lastLetterIndex - firstLetterIndex + 1);
+                    Array.Reverse(word,indexFirstLetter,indexLastLetter-indexFirstLetter+1);
                 }
             }
+            crypto.Append(word);
+            if (index < words.Length-1) 
+                crypto.Append(" ");
+            index++;
         }
+        Output = crypto.ToString();
     }
 }
+
