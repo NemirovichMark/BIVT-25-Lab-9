a363c02703549124cb6a1d40f0d62d4581585e1f
Liner <l123line@outlook.com>
2026-04-15 02:49:10 +0300

    PC

---
+++ Lab9/Purple/Task2.cs
@@ -0,0 +1,22 @@
+﻿using Lab9.Purple;
+
+namespace Lab9.Purple
+{
+    public class Task2 : Purple
+    {
+        public string[] Output;
+        public Task2(string text) : base(text)
+        {
+            Review();
+        }
+        public override void Review()
+        {
+            string[] result = new string[0];
+            
+        }
+        public override string ToString()
+        {
+            return _text;
+        }
+    }
+}
---
+++ Lab9/Purple/Task3.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Lab9.Purple
+{
+    public class Task3 : Purple
+    {
+        public (string, char)[] Codes;
+        public Task3(string text) : base(text)
+        {
+            FindRepeatable(_text, 5);
+        }
+        public override void Review()
+        {
+            
+        }
+        private string[] FindRepeatable(string text, int n)
+        {
+            string[] result = new string[n];
+            //Знаками препинания считать: '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/'.
+            char[] signs = { '.', '!', '?', ',', ':', '\"', ';', '–', '(', ')', '[', ']', '{', '}', '/', ' ' };
+            string format = text;
+            foreach (char el in signs)
+                format = format.Replace(el.ToString(), "");
+
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            for(int i = 0; i < format.Length - 1; i++)
+            {
+                string key = format[i..(i + 2)];
+                if (counts.ContainsKey(key))
+                    counts[key]++;
+                else
+                    counts[key] = 1;
+            }
+            string[] topN = counts.OrderBy(x => x.Value).Take(n).Select(x => x.Key).ToArray();
+
+            return topN;
+        }
+    }
+}
