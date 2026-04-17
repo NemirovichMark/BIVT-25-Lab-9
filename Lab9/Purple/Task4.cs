19099773e476e9571fa3aa380e7e53c36ad6ddd7
Artyom Nikityuk <a.k.nikityuk@gmail.com>
2026-04-17 22:13:38 +0300

    Add solution for tasks

---
+++ Lab9/Purple/Task4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Purple
+{
+    public class Task4 : Purple
+    {
+        private (string, char)[] _codes;
+        private string _output;
+
+        private (string, char)[] Codes => _codes.ToArray();
+        public string Output => _output;
+        public Task4(string input, (string, char)[] codes) : base(input)
+        {
+            _codes = codes;
+        }
+
+        public override void Review()
+        {
+            string res = Input; 
+            for (int i = 0; i < Codes.Length; i++)
+            {
+                string code = Codes[i].Item1;
+                string replace = "";
+                replace += Codes[i].Item2;
+
+                res = res.Replace(replace, code);
+            }
+
+            _output = res;
+        }
+
+        public override string ToString()
+        {
+            Review();
+            return _output;
+
+        }
+    }
+}
