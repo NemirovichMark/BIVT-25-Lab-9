5731092a039eee85b6b0db565fe6e9dd3b937ae2
korneyevvitya-debug <korneyev.vitya@gmail.com>
2026-04-19 21:28:48 +0300

    Add files via upload

---
+++ Lab9/Purple/Task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Purple
+{
+    public class Task1 : Purple
+    {
+        public string Output {  get; private set; }
+        public Task1(string input) : base(input)
+        {
+            Output = String.Empty;
+        }
+
+        public override void Review()
+        {
+            Output = Input;
+            int leftIndex = 0, rightIndex = 0;
+            for (int i = 1; i<Input.Length; i++)
+            {
+                if (IsInWord(Input[i]) && !IsInWord(Input[i - 1]))
+                    leftIndex = i;
+                else if (IsInWord(Input[i - 1]) && !IsInWord(Input[i]))
+                {
+                    rightIndex = i;
+                    string word = Output.Substring(leftIndex, rightIndex - leftIndex);
+                    if (!char.IsDigit(word[0]))
+                        Output = String.Concat(Output.Substring(0, leftIndex), ReverseString(word), Output.Substring(rightIndex));
+                }
+                    
+            }
+            
+        }
+        public override string ToString()
+        {
+            return Output;
+        }
+        private string ReverseString(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+        private bool IsInWord(char ch)
+        {
+            if (char.IsLetter(ch) || ch== '-' || ch== '\'' || char.IsDigit(ch))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Lab9/Purple/Task2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Purple
+{
+    public class Task2 : Purple
+    {
+        private string[] _output;
+        public string[] Output => _output.ToArray();
+        public Task2(string input) : base(input)
+        {
+            _output = new string[0];
+        }
+
+        public override void Review()
+        {
+            string[] words = Input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] result = new string[0];
+            int charcount = 0;
+            foreach (string word in words)
+            {
+                if (result.Length == 0 || charcount + result.Length + word.Length <= 50)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[^1] = word;
+                    charcount += word.Length;
+                }
+                else
+                {
+                    Normalise(result, charcount);
+                    charcount = word.Length;
+                    result = new string[]{word};
+                }
+            }
+            Normalise(result, charcount);
+        }
+        private void Normalise(string[] array, int length)
+        {
+            if (array.Length > 1)
+            {
+                for (int i = 0; i < 50 - length; i++)
+                {
+                    array[i % (array.Length - 1)] += " ";
+                }
+            }
+            Array.Resize(ref _output, _output.Length + 1);
+            _output[^1] = String.Concat(array);
+        }
+        public override string ToString()
+        {
+            return String.Join(Environment.NewLine, Output);
+        }
+    }
+}
