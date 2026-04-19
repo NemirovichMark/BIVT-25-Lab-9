ad9201c080279bf0bf0772c3b1bea4486c85ff91
korneyevvitya-debug <korneyev.vitya@gmail.com>
2026-04-19 21:27:51 +0300

    Update Purple.cs

--- Lab9/Purple/Purple.cs
+++ Lab9/Purple/Purple.cs
@@ -1,3 +1,17 @@
 namespace Lab9.Purple
 {
+    public abstract class Purple
+    {
+        protected Purple(string input)
+        {
+            Input = input;
+        }
+        public string Input { get; private set; }
+        public abstract void Review();
+        public virtual void ChangeText(string text)
+        {
+            Input = text;
+            Review();
+        }
+    }
 }
