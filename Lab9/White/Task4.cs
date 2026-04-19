aa0427af0bae1a13a9241d49b58aace00acbd6ae
ashurov18 <ashurovburhoniddin08@gmail.com>
2026-04-19 23:44:58 +0300

    Update Task4.cs

--- Lab9/White/Task4.cs
+++ Lab9/White/Task4.cs
@@ -1,46 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Lab9.White
 {
     public class Task4 : White
     {
         private int _output;
-        public int Output => _output;
-        public Task4(string input) : base(input)
+
+        public Task4(string text) : base(text)
         {
-            _output = 0;
         }
+
+        public int Output
+        {
+            get
+            {
+                return _output;
+            }
+        }
+
         public override void Review()
         {
-            // Если текст null, сумма должна быть 0
-            if (string.IsNullOrEmpty(_input))
-            {
-                _output = 0;
-                return;
-            }
+            _output = CalculateSumOfDigits(Input);
+        }
 
+        private int CalculateSumOfDigits(string text)
+        {
             int sum = 0;
-            // проходим по всему тексту _input
-            for (int i = 0; i < _input.Length; i++)
-            {
-                char ch = _input[i];
 
-                if (ch >= '0' && ch <= '9')
+            foreach (char ch in text)
+            {
+                if (char.IsDigit(ch))
                 {
-                    // Превращаем символ в число через ASCII-код
-                    sum += (int)(ch - '0');
+                    sum += ch - '0';
                 }
             }
 
-            _output = sum;
+            return sum;
         }
+
         public override string ToString()
         {
-            return _output.ToString();
+            return Output.ToString();
         }
     }
 }
