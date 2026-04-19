f93960c5a567ca759db9caf06bceda8ba193ba24
maria7987897 <denimd240@mail.ru>
2026-04-19 21:29:08 +0300

    White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,18 +1,15 @@
-using System;
-
-namespace Lab9
+namespace Lab9.White
 {
     public abstract class White
     {
         public string Input { get; private set; }
-
+    
         protected White(string input)
         {
             Input = input;
         }
-
         public abstract void Review();
-
+    
         public virtual void ChangeText(string text)
         {
             Input = text;
