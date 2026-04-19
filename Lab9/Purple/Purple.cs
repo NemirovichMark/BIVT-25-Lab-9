5068e98bb27e37b7fae7e5191886165a192f4092
abcdegen <141837183+abcdegen@users.noreply.github.com>
2026-04-19 22:47:56 +0300

    Update Purple.cs

--- Lab9/Purple/Purple.cs
+++ Lab9/Purple/Purple.cs
@@ -1,3 +1,22 @@
 namespace Lab9.Purple
 {
+    public abstract class Purple
+    {
+        private string _str;
+
+        public string Input => _str;
+        
+        protected Purple(string str)
+        {
+            _str = str;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            _str = text;
+            Review();
+        }
+    }
 }
