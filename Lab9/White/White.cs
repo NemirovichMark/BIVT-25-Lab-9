57838b5a944d4810a9ec40801528b773dc3be464
AlenaNovakh <alenanovakh@yandex.ru>
2026-04-20 15:30:56 +0300

    Update White.cs

--- Lab9/White/White.cs
+++ Lab9/White/White.cs
@@ -1,32 +1,23 @@
+﻿namespace Lab9.White
 namespace Lab9.White
 {
-    public abstract class White
+}
+
+public abstract class White
+{
+    public string Input { get; private set; }
+
+    protected White(string input)
     {
-        private string _input;
-        private string _text;
-
-        public string Input => _input;
-
-        protected White(string input)
-        {
-            _input = input;
-            _text = input;
-        }
-
-        protected string Text
-        {
-            get => _text;
-            set => _text = value;
-        }
-
-        public abstract void Review();
-
-        public virtual void ChangeText(string text)
-        {
-            _text = text;
-            Review();
-        }
-
-        public abstract override string ToString();
+        Input = input;
     }
+
+    public abstract void Review();
+
+    public virtual void ChangeText(string text)
+    {
+        Input = text;
+        Review();
+    }
+}
 }
