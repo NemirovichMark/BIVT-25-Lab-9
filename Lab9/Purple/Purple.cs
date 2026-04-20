92cdc425232f2c446f3a36633d68cf38da7a54b2
KiraFlux <pret2titio1n@gmail.com>
2026-04-20 10:27:46 +0300

    impl: Purple

--- Lab9/Purple/Purple.cs
+++ Lab9/Purple/Purple.cs
@@ -1,3 +1,16 @@
 namespace Lab9.Purple
 {
+    public abstract class Purple(string text)
+    {
+        public string Input { get; private set; } = text;
+        public string? Output { get; protected set; }
+
+        public abstract void Review();
+
+        public void ChangeText(string text)
+        {
+            Input = text;
+            Review();
+        }
+    }
 }
