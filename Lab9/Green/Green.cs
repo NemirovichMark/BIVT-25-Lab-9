e1a2239f895cda6c06c7ae7bba16d5862fc2376b
mitarayushka <126961692+mitarayushka@users.noreply.github.com>
2026-05-08 00:06:43 +0300

    Update Green.cs

--- Lab9/Green/Green.cs
+++ Lab9/Green/Green.cs
@@ -1,3 +1,20 @@
 ﻿namespace Lab9.Green
 {
+    public abstract class Green
+    {
+        private string _input;
+        public string Input => _input;
+        
+        protected  Green(string text)
+        {
+            _input = text;
+        }
+
+        public abstract void Review();
+        public virtual void ChangeText(string text)
+        {
+            _input = text;
+            Review();
+        }
+    }
 }
